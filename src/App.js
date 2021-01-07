8943dab57c8a6af3418cbd98fbb55738dde80f0b
juha-ylonen-futurice <juha.ylonen@futurice.com>
2021-01-07 23:53:19 +0200

    HelsinkiGrotesk

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 //import logo from './logo.svg';
 import './App.css';
+import './fonts.css';
+
 import Blog from './Blog';
+import {ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const groteskTheme = createMuiTheme({
+  typography: {
+    "fontFamily": "HelsinkiGrotesk",
+    "fontSize": 14,
+    "fontWeightLight": 300,
+    "fontWeightRegular": 400,
+    "fontWeightMedium": 500
+  },
+  overrides: {
+    MuiCssBaseline: {
+       body: {
+         backgroundColor: "#fff",
+         fontFamily: 'HelsinkiGrotesk',
+        },
+      },
+    },
+  });
 
 function App() {
   return (
-
+    <ThemeProvider theme={groteskTheme}>
       <Blog></Blog>
-
+    </ThemeProvider>
   );
 }
 
--- src/Hero.js
+++ src/Hero.js
@@ -18,41 +18,23 @@ import {
   FiCardMedia
 } from "./FullImageCard";
 
-/*
-import grotesk from './HelsinkiGrotesk-Regular.woff';
-
-const raleway = {
-  fontFamily: 'HelsinkiGrotesk',
-  fontStyle: 'normal',
-  fontDisplay: 'swap',
-  fontWeight: 400,
-  src: `
-    local('HelsinkiGrotesk'),
-    local('HelsinkiGrotesk-Regular'),
-    url(${grotesk}) format('woff2')
-  `,
-  unicodeRange:
-    'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
-};
-*/
-
 const useStyles = makeStyles({
   container: {
-    sheight: 550
+    height: 550
   },
   card: {
     backgroundColor: '#D0E6F7'
   },
   media: {
-    sheight: 550
+    height: 550
   },
  fiCardContent: {
     height: 550,
     color: 'black',
-    width: '40%'
+    width: '40%',
   },
   fiCardContentTextSecondary: {
-    color: "black"
+    color: "black",
   },
   mediaPic : {
     clipPath: 'url(#koros)'
