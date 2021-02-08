4bd2764640539bdaadc9baa112ab1cf457d8db47
juha-ylonen-futurice <juha.ylonen@futurice.com>
2021-02-08 23:31:51 +0200

    colors from taxonomies

---
+++ src/colorHelper.js
@@ -0,0 +1,30 @@
+import {findTaxonomyValue} from "./taxonomiesHelper.js";
+let _ = require('lodash');
+
+const colors = [
+  {name: 'musta', rgb: '#000'},
+  {name: 'valkoinen', rgb: '#fff'},
+  {name: 'musta-05', rgb: '#f1f1f1'},
+  {name: 'musta-20', rgb: '#ccc'},
+  {name: 'musta-60', rgb: '#666'},
+  {name: 'bussi', rgb: '#000bf'},
+  {name: 'sumu', rgb: '#9fc9eb'},
+  {name: 'metro', rgb: '#fd4f00'},
+  {name: 'suomenlinna', rgb: '#f5a3c7'},
+  {name: 'suomenlinna-medium-light', rgb: '#ffdbeb'},
+];
+
+export const getColor = (item, field, taxonomies) => {
+  try {
+    const id = item.relationships[field].data.id;
+    const name = findTaxonomyValue(id, taxonomies);
+    const c = _.find(colors, {name: name}).rgb;
+    if (!c) {
+      console.log('undefined color ' + id + ' ' + name);
+    }
+    return c;
+  } catch(error) {
+
+  }
+  return "not-found";
+}
---
+++ src/taxonomiesHelper.js
@@ -0,0 +1,59 @@
+let _ = require('lodash');
+
+// Taxonomy data is in: .included
+export const findTaxonomy = (data, field) => {
+  console.log(data.included);
+
+  try {
+    let type = data.data[0].relationships[field].data.type;
+    let taxonomy = _.find(data.included, {type: type});
+    if (taxonomy) {
+      return taxonomy.attributes.name;
+    }
+  } catch(error) {
+    console.log(field);
+    console.log(error);
+  }
+
+  return "";
+}
+
+export const findTaxonomyValue = (id, taxonomies) => {
+  try {
+    const name = _.find(taxonomies, {id: id}).name;
+    return name;
+  } catch(error) {
+    console.log("taxonomies error: " + id);
+  }
+  return "no-value";
+}
+
+export const setTaxonomies = (taxonomiesRaw) => {
+  let tax = [];
+  try {
+    taxonomiesRaw.map((item, index) => {
+      item[1].data.data.map((t, i) => {
+        tax.push({
+          category: item[0],
+          type: t.type,
+          name: t.attributes.name,
+          id: t.id,
+        });
+        return tax;
+      });
+      return tax;
+    });
+  } catch(error) {
+    console.log('Taxonomies');
+    console.log(error);
+  }
+  return tax;
+}
+
+export const getTaxonomyPath = (site, taxonomy, secondPage) => {
+  let path = site + '/apijson/taxonomy_term/'+taxonomy+'?fields[taxonomy_term--'+taxonomy+']=id,name';
+  if (secondPage) {
+    path += "&page[offset]=50&page[limit]=50";
+  }
+  return path;
+}
