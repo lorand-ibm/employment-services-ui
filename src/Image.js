36bf43a6109c60e0025cfab6f5f8b200aa8cbf19
juha-ylonen-futurice <juha.ylonen@futurice.com>
2021-02-08 12:46:57 +0200

    cleanup

--- src/Image.js
+++ src/Image.js
@@ -1,41 +1,10 @@
-import PropTypes from "prop-types";
 import * as React from "react";
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import {Button} from "hds-react/components/Button";
 import {makeStyles} from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-//import "hds-core";
-import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
-  title: {
-    color: 'black',
-    background: 'white',
-    fontFamily: 'HelsinkiGrotesk',
-    borderColor: 'red',
-    marginBottom: 0,
-    height: 400,
-  },
-  text: {
-    color: 'black',
-    fontSize: 16,
-    fontFamily: 'HelsinkiGrotesk'
-  },
-  textArea: {
-    margin:0,
-    padding: 0,
-  },
-  button: {
-    color: 'black',
-    background: 'white',
-  },
-  icon: {
-    marginTop: 15,
-  },
   media: props => ({
     height: props.height ? props.height : 400,
   }),
@@ -43,14 +12,13 @@ const useStyles = makeStyles((theme) => ({
 
 function Image(props) {
     const classes = useStyles(props);
-    const { title, text, button_text, image, site, height } = props;
+    const { image, site } = props;
 
     const path = site + image;
 
-    console.log (height);
     return (
         <React.Fragment>
-          <Card className={classes.root}>
+          <Card>
             <CardActionArea>
               <CardMedia
                 className={classes.media}
@@ -64,8 +32,6 @@ function Image(props) {
 }
 
 Image.propTypes = {
-    text: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
 };
 
 
