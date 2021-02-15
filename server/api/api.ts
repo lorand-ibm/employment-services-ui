41b78015a3063252262302b54351d844526983c0
Antti Haapaniemi <antti.haapaniemi@futurice.com>
2021-02-15 18:13:47 +0200

    Events API

--- server/api/api.ts
+++ server/api/api.ts
@@ -1,6 +1,10 @@
 import {Router} from 'express';
 
+import eventsRouter from './events';
+
 const apiRouter = Router();
+
+apiRouter.use("/events", eventsRouter)
 
 // TODO
 apiRouter.get("/search/:query", (req, res) => {
