6ec6c5a34d4c374fd587a48ddac601e95198f1d3
Antti Haapaniemi <antti.haapaniemi@futurice.com>
2021-03-12 15:38:41 +0200

    Initial event sync

---
+++ sync/index.ts
@@ -0,0 +1,29 @@
+import dotEnv from "dotenv";
+import path from "path";
+import cron from "node-cron";
+
+import { syncElasticSearch } from "./syncEvents";
+import { syncLinkedEvents } from "./linkedEventsSync";
+
+dotEnv.config({ path: path.resolve(__dirname + "/../.env") });
+
+const syncEvents = async () => {
+  console.log("------");
+  console.log("start", new Date());
+  console.log("SYNC LINKED EVENTS");
+  const modified = await syncLinkedEvents();
+  if (modified) {
+    console.log("SYNC ELASTICSEARCH");
+    await syncElasticSearch();
+  } else {
+    console.log("SYNC ELASTICSEARCH");
+    console.log("nothing to do");
+  }
+};
+
+syncEvents();
+
+// Sync every hour
+cron.schedule("0 * * * *", async () => {
+  syncEvents();
+});
---
+++ sync/syncEvents.ts
@@ -0,0 +1,75 @@
+import { getClient } from "./elasticsearchClient";
+import axios from "axios";
+
+async function fetchDrupalEvents() {
+  const drupalUrl = process.env.DRUPAL_URL;
+  if (!drupalUrl) {
+    throw "Set DRUPAL_URL";
+  }
+  const eventsDrupalUrl = drupalUrl + "/fi/apijson/node/event?include=field_page_content";
+  const res = await axios.get(eventsDrupalUrl);
+
+  const data = res.data;
+  if (!data) {
+    throw "Error fetcing drupal events, no data in res";
+  }
+
+  const eventData: Array<any> = data.data;
+  if (!eventData) {
+    throw "Error fetcing drupal events, no event data in res";
+  }
+
+  const parsedEvents = eventData.reduce((acc: any, curr: any) => {
+    const attr = curr.attributes;
+    const event = {
+      path: attr.path.alias,
+      title: attr.field_title,
+      image: attr.field_image_url,
+      text: attr.field_text?.value,
+      startTime: attr.field_start_time,
+      endTime: attr.field_end_time,
+    };
+    return [...acc, event];
+  }, []);
+
+  return parsedEvents;
+}
+
+export const syncElasticSearch = async () => {
+  const client = getClient();
+  try {
+    await client.indices.delete({ index: "events" });
+  } catch (err) {
+    console.warn("WARNING when deleting 'events' index:");
+  }
+
+  try {
+    await client.indices.create(
+      {
+        index: "events",
+        body: {
+          mappings: {
+            properties: {
+              path: { type: "text" },
+              title: { type: "text" },
+              image: { type: "text" },
+              text: { type: "text" },
+              startTime: { type: "date" },
+              endTime: { type: "date" },
+            },
+          },
+        },
+      },
+      { ignore: [400] }
+    );
+  } catch (err) {
+    console.log("ERROR", err);
+    return;
+  }
+
+  const dataset = await fetchDrupalEvents();
+  const body = dataset.flatMap((doc: any) => [{ index: { _index: "events" } }, doc]);
+  const { body: bulkResponse } = await client.bulk({ refresh: true, body });
+  const { body: count } = await client.count({ index: "events" });
+  console.log("added:", count.count);
+};
