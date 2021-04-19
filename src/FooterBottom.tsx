e5fd9622689c32c0227b03d46f1ed90cedfc71f9
Jussi Leskinen <jussi.leskinen@siili.com>
2021-04-19 12:44:10 +0300

    Add link to footer. Improve cookie banner.

--- src/FooterBottom.tsx
+++ src/FooterBottom.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import { Footer } from  "hds-react/components/Footer";
+import { useTranslation } from "react-i18next";
 import { drupalUrl } from "./config";
 import { Lang } from "./types";
-
-
-const footerTexts = {
-  fi: { feedback: 'Anna palautetta', feedbackLink: 'https://www.hel.fi/helsinki/fi/kaupunki-ja-hallinto/osallistu-ja-vaikuta/palaute/', goup: 'Sivun alkuun', accessibility: 'Saavutettavuusseloste'},
-  sv: { feedback: 'Ge respons', feedbackLink: 'https://www.hel.fi/helsinki/sv/stad-och-forvaltning/delta/feedback', goup: 'Till början av sidan', accessibility: 'Tillgänglighetsutlåtande'},
-  en: { feedback: 'Give feedback', feedbackLink: 'https://www.hel.fi/helsinki/en/administration/participate/feedback', goup: 'Back to the top', accessibility: 'Accessibility statement'},
-}
 
 const useStyles = makeStyles((theme) => ({
   footerWrapper: {
@@ -25,21 +20,9 @@ interface FooterProps {
 
 function FooterBottom(props: FooterProps) {
   const classes = useStyles();
+  const { t } = useTranslation();
+  const history = useHistory();
   const { title, lang, lastParagraphColor = '' } = props;
-
-  let texts = footerTexts.en;
-  switch(lang) {
-    case 'sv':
-      texts = footerTexts.sv;
-      break;
-    case 'en':
-      texts = footerTexts.en;
-      break;
-    case 'fi':
-    default:
-      texts = footerTexts.fi;
-      break;
-  }
 
   const scrollToTop = (event: any) => {
     event.preventDefault();
@@ -48,6 +31,12 @@ function FooterBottom(props: FooterProps) {
       behavior: "smooth"
     });
   };
+
+  const deleteCookie = (event: any, name: string) => {
+    event.preventDefault();
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    history.go(0)
+  }
 
   return (
     <>
@@ -64,9 +53,13 @@ function FooterBottom(props: FooterProps) {
         >
           <Footer.Navigation navigationAriaLabel="{title} navigation"></Footer.Navigation>
           <Footer.Base copyrightHolder="Copyright" copyrightText="All rights reserved">
-              <Footer.Item href={`${drupalUrl}/sites/default/files/2021-02/saavutettavuusseloste%2026.2.2021.pdf`} label={texts.accessibility} />
-              <Footer.Item href={texts.feedbackLink} label={texts.feedback} />
-              <Footer.Item href="#" label={texts.goup} onClick={scrollToTop} />
+            <Footer.Item href={`${drupalUrl}/sites/default/files/2021-02/saavutettavuusseloste%2026.2.2021.pdf`} label={t("footer.accessibility")} />
+            <Footer.Item href={t("cookies.url")} label={t("footer.cookies")} />
+            <Footer.Item href="#" label={t("footer.cookie_settings")} onClick={(e: any) => {
+              deleteCookie(e ,'tyollisyyspalvelut_cookie_consent');
+            }}/>
+            <Footer.Item href={t("footer.feedbackLink")} label={t("footer.feedback")} />
+            <Footer.Item href="#" label={t("footer.goup")} onClick={scrollToTop} />
           </Footer.Base>
         </Footer>
       </div>
