a98face4b0ae98f36a528120d22d9e6992f33fc1
Jussiles <juss.leskinen@gmail.com>
2021-05-25 16:02:21 +0300

    Add icons to wysiwyg links.

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -6,7 +6,7 @@ function Link({ url, text }: { url: string, text: string }) {
   const isExternalLink = url && (url.startsWith("https://") || url.startsWith("https://"));
   return (
     <a href={url} style={{ display: "flex", fontSize: "16px", textDecoration: "none", fontFamily: "HelsinkiGrotesk" }}>
-      <div style={{ padding: "1px 5px 0 0" }}>{text}</div>
+      <span style={{ padding: "1px 5px 0 0" }}>{text}</span>
       {isExternalLink ? <IconLinkExternal /> : <IconArrowRight />}
     </a>
   );
