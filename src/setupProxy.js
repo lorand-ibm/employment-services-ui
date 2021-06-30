ba1ff06955acb0a46e966f1ef8eb0af553d3fbb2
Jussiles <juss.leskinen@gmail.com>
2021-06-30 14:18:49 +0300

    Change SSR path based on env.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,14 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const path = '/*';
+const __DEV__ = process.env.NODE_ENV === 'development';
+
+if (__DEV__) {
+  path = '/api'
+}
 
 module.exports = function(app) {
   app.use(
-    '/*',
+    path,
     createProxyMiddleware({
       target: 'http://localhost:9000',
       changeOrigin: true,
