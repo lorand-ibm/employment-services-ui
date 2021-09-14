75695a7cfb2fdfbd10bca02e9bc9c9718438b912
Jussiles <juss.leskinen@gmail.com>
2021-09-14 12:49:27 +0300

    Fixes for overflowing content.

--- src/components/SingleCard.tsx
+++ src/components/SingleCard.tsx
@@ -37,6 +37,7 @@ const useStyles = makeStyles((theme: any) => ({
     fontWeight: "bold",
     '& a': {
       color: 'inherit',
+      wordBreak: 'break-word',
       '&:hover': {
         textDecoration: 'none',
       }
@@ -47,6 +48,9 @@ const useStyles = makeStyles((theme: any) => ({
     fontFamily: "HelsinkiGrotesk",
     backgroundColor: props.bgColor,
     color: props.text_color,
+    "& li": {
+      wordBreak: "break-word",
+    },
   }),
   button: (props: SingleCardProps) => ({
     fontFamily: "HelsinkiGrotesk",
