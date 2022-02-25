c00b3b4b2d3fe91d2c2669f335e691814c8a7947
Jussiles <juss.leskinen@gmail.com>
2022-02-25 15:32:47 +0200

    Temp fixing typing to get started.

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,21 +2,25 @@ import Link from "next/link"
 
 import { PreviewAlert } from "@/components/preview-alert"
 
-export function Layout({ children }) {
+interface LayoutProps {
+  children: any;
+}
+
+export function Layout({ children }: LayoutProps): JSX.Element {
   return (
     <>
       <PreviewAlert />
-      <div className="max-w-screen-md px-6 mx-auto">
+      <div>
         <header>
-          <div className="container flex items-center justify-between py-6 mx-auto">
+          <div>
             <Link href="/" passHref>
-              <a className="text-2xl font-semibold no-underline">
+              <a>
                 Next.js for Drupal
               </a>
             </Link>
           </div>
         </header>
-        <main className="container py-10 mx-auto">{children}</main>
+        <main>{children}</main>
       </div>
     </>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head"
-import { GetStaticPropsResult } from "next"
+import { GetStaticPropsContext, GetStaticPropsResult } from "next"
 import { DrupalNode, getResourceCollectionFromContext, getResourceByPath } from "next-drupal"
 import { Layout } from "@/components/layout"
 
@@ -7,7 +7,7 @@ interface HomePageProps {
   node: DrupalNode
 }
 
-export async function getStaticProps(context): Promise<GetStaticPropsResult<HomePageProps>> {
+export async function getStaticProps(context: GetStaticPropsContext): Promise<GetStaticPropsResult<HomePageProps>> {
   const { locale } = context
   const node = await getResourceByPath<DrupalNode>(`${locale}/landingpage`);
 
