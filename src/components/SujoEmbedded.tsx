52c278ed6b7bb7342719d573dd28147318ee58c8
Jussiles <juss.leskinen@gmail.com>
2022-04-20 10:37:35 +0300

    Update sujo urls.

--- src/components/SujoEmbedded.tsx
+++ src/components/SujoEmbedded.tsx
@@ -9,8 +9,8 @@ function SujoEmbedded(props: { training: boolean }): JSX.Element {
         title="Sujo"
         src={
           training
-            ? "https://www.sujo.fi/pls/sujo/tyoko.training"
-            : "https://www.sujo.fi/pls/sujo/tyoko.salary"
+            ? "https://tyopa.sujo.fi/pls/sujo/tyoko.training"
+            : "https://tyopa.sujo.fi/pls/sujo/tyoko.salary"
         }
         height="800px"
         width="100%"
